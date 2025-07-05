9b8638fe6bf71865f95bd13e99530d7d1a613fb0
daiki <daikihi+github@gmail.com>
2025-07-05 10:00:41 +0900

    remove unused comments

--- batchs/src/flower_loader/flower_loader_master_gateway.rs
+++ batchs/src/flower_loader/flower_loader_master_gateway.rs
@@ -1,8 +1,6 @@
 use log::error;
 
 pub fn load_master_data(file_name: &str) -> String {
-    // ここでマスターデータを読み込む処理を実装
-    // 例えば、ファイルからデータを読み込んで文字列として返す
     match std::fs::read_to_string(file_name) {
         Ok(content) => content,
         Err(e) => {
