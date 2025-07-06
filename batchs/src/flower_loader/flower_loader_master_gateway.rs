ad1270fed1ff136041b38e9cff0abc9e42a95b21
daiki <daikihi+github@gmail.com>
2025-07-06 12:09:01 +0900

    simple test for gateway

--- batchs/src/flower_loader/flower_loader_master_gateway.rs
+++ batchs/src/flower_loader/flower_loader_master_gateway.rs
@@ -10,3 +10,18 @@ pub fn load_master_data(file_name: &str) -> String {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+
+
+    #[test]
+    fn test_load_master_data() {
+        let test_file = "../resources/master_data/tests/test_data_flower.csv";
+        let content = load_master_data(test_file);
+        assert!(!content.is_empty(), "Failed to load master data");
+        assert_eq!(content.lines().count(), 3, "Unexpected number of lines in master data");
+    }
+}
