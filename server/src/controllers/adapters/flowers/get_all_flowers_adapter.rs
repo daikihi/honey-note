a1656f2f610f1ca4c88b080dadaba989bfacf8f9
daiki <daikihi+github@gmail.com>
2025-08-15 15:42:13 +0900

    get_all_flowers_response_adapter was not used

--- server/src/controllers/adapters/flowers/get_all_flowers_adapter.rs
+++ server/src/controllers/adapters/flowers/get_all_flowers_adapter.rs
@@ -1,6 +1,7 @@
 use common_type::models::flowers::Flower;
+use sqlx::{Pool, Sqlite};
 
-use crate::use_case::get_all_flowers::get_all_flowers_dto::GetAllFlowersRequestDto;
+use crate::use_case::get_all_flowers::{self, get_all_flowers_dto::GetAllFlowersRequestDto};
 
 pub fn get_all_flowers_request_adapter() -> Result<GetAllFlowersRequestDto, actix_web::Error> {
     // This function will handle the request to get all flowers
@@ -8,18 +9,21 @@ pub fn get_all_flowers_request_adapter() -> Result<GetAllFlowersRequestDto, acti
     // and return the response in the appropriate format.
 
     // Placeholder for actual implementation
-    let path = common::infrastructure::db::sqlx::DB_FILE_NAME;
-    let pool = common::infrastructure::db::sqlx::get_sqlite_pool(path.to_string());
+    let path: &str = common::infrastructure::db::sqlx::DB_FILE_NAME;
+    let pool: Pool<Sqlite> = common::infrastructure::db::sqlx::get_sqlite_pool(path.to_string());
     Ok(GetAllFlowersRequestDto { pool })
 }
 
 pub fn get_all_flowers_response_adapter(
-    response: Result<Vec<Flower>, common::errors::AppError>,
+    response: Result<
+        get_all_flowers::get_all_flowers_dto::GetAllFlowersResponseDto,
+        common::errors::AppError,
+    >,
 ) -> Result<actix_web::HttpResponse, actix_web::Error> {
     match response {
         Ok(flowers) => {
-            log::info!("Successfully fetched flowers: {:?}", flowers);
-            Ok(actix_web::HttpResponse::Ok().json(flowers))
+            log::info!("Successfully fetched flowers: {:?}", flowers.flowers);
+            Ok(actix_web::HttpResponse::Ok().json(flowers.flowers))
         }
         Err(e) => {
             log::error!("Error fetching flowers: {}", e);
